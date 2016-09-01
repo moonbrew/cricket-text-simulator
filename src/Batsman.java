8922cc1f056f3fc5fe19569a72bc5593e953821f
MoonBrew <jossyg@ymail.com>
2016-09-01 00:01:28 +0000

    Some formatting and new test

--- src/Batsman.java
+++ src/Batsman.java
@@ -1,4 +1,3 @@
-
 /**
  * Knows name, probabilities, runs scored and balls played.
  * Plays a ball based on probability.
@@ -6,87 +5,88 @@
 
 public class Batsman {
 
-	private String name;
-	private int[] probabilities;
-	private int runsScored;
-	private int ballsPlayed;
+    private String               name;
+    private int[]                probabilities;
+    private int                  runsScored;
+    private int                  ballsPlayed;
 
-	public static final String[] BALL_RESULT = {"dot ball", "1", "2", "3", "4", "5", "6", "out"};
-	private String currentBallResult;
+    public static final String[] BALL_RESULT = {"dot ball", "1", "2", "3", "4", "5", "6", "out"};
+    private String               currentBallResult;
 
-	/**
-	 * Construct a batsman given a name and probabilities.
-	 * @param n Name of batsman
-	 * @param p Probabilities in an integer array in the same order as Batsman.BALL_RESULT.
-	 */
-	public Batsman(String n, int[] p){
-		name = n;
-		probabilities = p;
-		//check same length as results(8)
-		//check no negative?
-		//check total 100
-	}
-
-	/**
-	 * Play a ball based on given probability. Updates balls played and runs scored.
-	 * @return ball result.
-	 */
-	public String playBall(){
-		double r = Math.random();
-		playBall(r);//body separated for testing
-		return currentBallResult;		
-	}
-		
-	private void playBall(double r){	
-		//System.out.println(r);//
-		int count = 0;
-		double sum = 0;
-		while(count < BALL_RESULT.length){
-			sum += (double) probabilities[count] / 100;//coz int
-			if(r < sum)
-				break;
-			count++;
-		}
-		currentBallResult = BALL_RESULT[count];
-		addRuns();
-		ballsPlayed++;
-	}
-	//remove
-	public void testPlayBall(double r){
-	    playBall(r);
+    /**
+     * Construct a batsman given a name and probabilities.
+     * @param n Name of batsman
+     * @param p Probabilities in an integer array in the same order as Batsman.BALL_RESULT.
+     */
+    public Batsman(String n, int[] p) {
+        name = n;
+        probabilities = p;
+        //check same length as results(8)
+        //check no negative?
+        //check total 100
     }
-	
-	//Helper method showing short way of getting runs that can have a different implementation
-	private void addRuns() {
-		if(!currentBallResult.equals("dot ball") && !currentBallResult.equals("out"))
-			runsScored += Integer.parseInt(currentBallResult);
-	}
 
-	/**
-	 * @return name of batsman.
-	 */
-	public String getName(){
-		return name;
-	}
+    /**
+     * Play a ball based on given probability. Updates balls played and runs scored.
+     * @return ball result.
+     */
+    public String playBall() {
+        double r = Math.random();
+        playBall(r);//body separated for testing
+        return currentBallResult;
+    }
 
-	/**
-	 * @return runs scored.
-	 */
-	public int getRunsScored(){
-		return runsScored;
-	}
+    private void playBall(double r) {
+        //System.out.println(r);//
+        int count = 0;
+        double sum = 0;
+        while (count < BALL_RESULT.length) {
+            sum += (double)probabilities[count] / 100;//coz int
+            if (r < sum)
+                break;
+            count++;
+        }
+        currentBallResult = BALL_RESULT[count];
+        addRuns();
+        ballsPlayed++;
+    }
 
-	/**
-	 * @return balls played by this batsman.
-	 */
-	public int getBallsPlayed(){
-		return ballsPlayed;
-	}
+    //remove
+    public void testPlayBall(double r) {
+        playBall(r);
+    }
 
-	/**
-	 * @return result of last ball played.
-	 */
-	public String getCurrentBallResult(){
-		return currentBallResult;
-	}
+    //Helper method showing short way of getting runs that can have a different implementation
+    private void addRuns() {
+        if (!currentBallResult.equals("dot ball") && !currentBallResult.equals("out"))
+            runsScored += Integer.parseInt(currentBallResult);
+    }
+
+    /**
+     * @return name of batsman.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return runs scored.
+     */
+    public int getRunsScored() {
+        return runsScored;
+    }
+
+    /**
+     * @return balls played by this batsman.
+     */
+    public int getBallsPlayed() {
+        return ballsPlayed;
+    }
+
+    /**
+     * @return result of last ball played.
+     */
+    public String getCurrentBallResult() {
+        return currentBallResult;
+    }
 }
--- src/TestBatsman.java
+++ src/TestBatsman.java
@@ -4,21 +4,52 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class TestBatsman {
-    private int[] values = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99};
-
 
     @Test
-    public void testPlayBall() {
-
+    public void testPlayBall1() {//checking 100%
+        int[] values = {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 99};
         Batsman a = new Batsman("Testguy", new int[]{0, 0, 0, 0, 0, 0, 0, 100});
         for (int x : values) {
-            a.testPlayBall((double)x/100);
+            a.testPlayBall((double)x / 100);
             assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[7])));
         }
-
-        Batsman b = new Batsman("Testguy", new int[]{});
-        Batsman c = new Batsman("Testguy", new int[]{});
-        Batsman d = new Batsman("Testguy", new int[]{});
     }
 
+    @Test
+    public void testPlayBall2() {//edge value, skipping 0s
+        Batsman a = new Batsman("Testguy", new int[]{0, 0, 0, 50, 0, 0, 50, 0});
+
+        a.testPlayBall(0);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[3])));
+        a.testPlayBall(0.49);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[3])));
+        a.testPlayBall(0.5);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[6])));
+        a.testPlayBall(0.75);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[6])));
+        a.testPlayBall(0.9999999);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[6])));
+    }
+
+    @Test
+    public void testPlayBall3() {///all results
+        Batsman a = new Batsman("Testguy", new int[]{10, 10, 20, 10, 10, 20, 10, 10});
+
+        a.testPlayBall(0.09);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[0])));
+        a.testPlayBall(0.19);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[1])));
+        a.testPlayBall(0.39);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[2])));
+        a.testPlayBall(0.49);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[3])));
+        a.testPlayBall(0.59);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[4])));
+        a.testPlayBall(0.79);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[5])));
+        a.testPlayBall(0.89);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[6])));
+        a.testPlayBall(0.99);
+        assertThat(a.getCurrentBallResult(), is((Batsman.BALL_RESULT[7])));
+    }
 }
