0b010156b895b5b00e836853e2e9296c03bf0c10
MoonBrew <jossyg@ymail.com>
2016-09-02 20:21:48 +0000

    Reformatting.

--- src/TestInnings.java
+++ src/TestInnings.java
@@ -4,15 +4,16 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
 public class TestInnings {
-    
+
     @Test
     public void testInitialState() {
-        Innings a = new Innings ("Testteam", 4);
-        assertThat (a.getRunsLeft(), is (0));// exception? Cant call?
-        assertThat (a.getBallsLeft(), is (24));
-        
-        assertThat (a.getWicketsLeft(), is (0));//exception add batsman. atleast 2?
-        assertThat (a.getBatsmanIterator(), is (0)); //ok at 0?
-        assertThat (a.getResult(), is (0)); // play innings exception
+        Innings a = new Innings("Testteam", 4);
+        assertThat("Runs", a.getRunsLeft(), is(0));// exception? Cant call?
+        assertThat("Balls", a.getBallsLeft(), is(24));
+
+        assertThat("Wickets", a.getWicketsLeft(), is(-1));//exception add batsman. atleast 2?
+        assertThat("Iterator", a.getBatsmanIterator().hasNext(), is(false)); //ok at 0? Yes
+        assertThat("Iterator", a.getBatsmanIterator().next(), is(0)); //throws no such element.
+        assertThat("Result", a.getResult(), is(0)); // play innings exception unsupported operation exception
     }
 }
